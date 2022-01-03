523fcbb1348418802d4a4f536ac7357ca5f85d05
greenstar1151 <39073901+greenstar1151@users.noreply.github.com>
2022-01-03 22:14:20 +0900

    Add entrypoint

--- src/main.py
+++ src/main.py
@@ -1,22 +1,10 @@
-import requests
+from costco import costco
+from emart import emart
+from traders import traders
 
 
-# col
-# id, base_date, mart_type, mart_name, loc, start_time, end_time, next_holiday
 
-# mart_type
-# emart, traders, homeplus, costco, emart_everyday
-
-BASE_URL = {
-    'emart': 'https://store.emart.com/branch/searchList.do',
-    'homeplus': 'https://corporate.homeplus.co.kr/STORE/HyperMarket.aspx',
-    'costco': 'https://www.costco.co.kr/store-finder/search?q=',
-    'emart_everyday_list': 'http://www.emarteveryday.co.kr/branch/searchBranch.jsp',
-    'emart_everyday_info': 'http://www.emarteveryday.co.kr/branch/branchView.jsp'
-}
-
-
-response = requests.get(BASE_URL['costco'], data={})
-
-print(len(response.json()['data']))
-
+if __name__ == '__main__':
+    costco()
+    emart()
+    traders()
